92faeb1d8d88ff8b50ec1c4014f5670fb15b0a6d
Miguel de Oliveira Santos <miguel.santos6700@gmail.com>
2022-01-29 18:51:27 -0300

    created function for changed href of links

---
+++ script.js
@@ -0,0 +1,17 @@
+const linksSocialMedia = {
+  github: 'miguelsantos1',
+  youtube: 'UC_zIplc9EgMNz4xNJyACw8Q',
+  twitter: 'miguelo03164369',
+  instagram: 'mgy1kk',
+  facebook: ''
+}
+
+function changeSocialMediaLinks() {
+  for(let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks();
