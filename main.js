7f80ce384dc9f0889c28d2a2b6a7eddc9420fa65
Miguel de Oliveira Santos <miguel.santos6700@gmail.com>
2022-01-30 17:59:56 -0300

    Creating a function for get github API information for modify elements in index.html

--- main.js
+++ main.js
@@ -13,4 +13,18 @@ function changeSocialMediaLinks() {
     li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
   }
 }
+function getGithubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+  fetch(url).then(response => response.json()) //Getting information of github and changing for json
+  .then(data => { 
+    userPhotoGithub.src = data.avatar_url 
+    userNameGithub.textContent = data.name
+    userLinkGithub.href = data.html_url
+    userLoginGithub.textContent = data.login
+    userBioGithub.textContent = data.bio
+  }) 
+}
+
+// Calling functions
 changeSocialMediaLinks();
+getGithubProfileInfos();
